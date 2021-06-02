6393fd281357df04a681c017c7cffab6a50da28f
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2021-06-01 22:07:28 -0400

    Initial Commit

---
+++ src/main/java/com/neeraj/preperation/JavaPrepApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.preperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPrepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neeraj/preperation/JavaPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.preperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
